c15407133940841cf505763af98b6965738c2c19
SusanaSantosM <ssantosmallqui@danielcastelao.org>
2025-02-05 11:12:27 +0100

    Probamos la conexión del servidor con el cliente.

--- Calculadora/src/Servidor.java
+++ Calculadora/src/Servidor.java
@@ -1,2 +1,40 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Servidor {
+    private static int puerto = 5000;
+
+    public static void main(String[] arg){
+        try{
+            //Creamos el socket del servidor
+            ServerSocket socketServidor = new ServerSocket(puerto);
+            System.out.println("Servidor iniciado en el puerto " + puerto);
+
+            while(true){
+                //Aceptamos conexiones con el cliente
+                Socket socket = socketServidor.accept();
+                System.out.println("Cliente conectado "+socket.getInetAddress());
+
+                //Creamos los buffers de entrada y salida
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+                //Leemos la petición del cliente
+                String peticion = in.readLine();
+                System.out.println("Petición del cliente: "+peticion);
+
+                //Enviamos la respuesta al cliente
+                out.println("Hola, cliente");
+
+                //Cerramos la conexión
+                socket.close();
+                System.out.println("Cliente desconectado");
+            }
+        }catch(Exception e){
+            System.out.println("Error al iniciar el servidor"+e.getMessage());
+        }
+    }
 }
