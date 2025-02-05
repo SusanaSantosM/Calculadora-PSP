fb4d1226acc1a3357840515d5b7ff84fd3ecd7c6
SusanaSantosM <ssantosmallqui@danielcastelao.org>
2025-02-05 15:49:58 +0100

    Creamos el método para operar los numeros del cliente

--- Calculadora/src/Servidor.java
+++ Calculadora/src/Servidor.java
@@ -9,7 +9,28 @@ public class Servidor {
     private static int puerto = 5000;
     private static double resultado;
 
-    
+    public static double resultadoOperacion(double num1, double num2, String operador){
+        // Según el operador, se cumple cada caso
+        switch (operador){
+            case "+":
+                return num1 + num2;
+            case "-":
+                return num1 - num2;
+            case "*":
+                return num1 * num2;
+            case "/":
+                if (num2 == 0){
+                    try {
+                        throw new Exception("No se puede dividir entre 0");
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+                return num1 / num2;
+            default:
+                throw new IllegalArgumentException("Operador no válido");
+        }
+    }
 
     public static void main(String[] arg){
         try{
